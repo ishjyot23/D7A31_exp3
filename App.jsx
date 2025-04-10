5891ba4757a062eac2cfe007167ab5731d67432c
ishjyot23 <2023.ishjyot.kaur@ves.ac.in>
2025-04-10 16:47:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import TodoList from './components/TodoList';
+
+function App() {
+    return (
+        <div>
+            <h1>My To-Do App</h1>
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
